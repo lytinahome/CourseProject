11069b45e3d137bb03e9c9e4fcfc11e77b7fce57
qjwcao <43865938+cqjw@users.noreply.github.com>
2022-12-07 22:36:36 -0800

    organized files

--- step_1_convert_data_2_csv.py
+++
@@ -1,9 +0,0 @@
-import json
-import pandas as pd
-data_file = open(r"c:/Users/Yu/Desktop/cs410/final/yelp_academic_dataset_review.json", encoding = "utf8", errors ='replace')
-data = []
-for line in data_file:
-    data.append(json.loads(line))
-checkin_df = pd.DataFrame(data)
-checkin_df.to_csv("c:/Users/Yu/Desktop/cs410/final/review.csv")
-data_file.close()
